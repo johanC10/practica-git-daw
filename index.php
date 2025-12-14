3839490637b4d66786ae968a5ad74ad7925c54b7
johanC10 <jestupina@inscaparrella.cat>
2025-12-14 22:22:31 +0100

    feat: Añadir lógica de tareas

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 // Sistema de Gestió de Tasques
 // Versió 1.0
+function mostrarTasques($tasques) {
+    echo "=== LLISTA DE TASQUES ===\n";
+    foreach ($tasques as $id => $t) { echo "[$id] $t\n"; }
+}
+$tasques = ["Comprar pa", "Estudiar Git"];
 echo "=== GESTOR DE TASQUES ===\n";
-echo "Benvingut al sistema!\n";
+echo "Benvingut al sistema!\n\n";
+mostrarTasques($tasques);
 ?>
